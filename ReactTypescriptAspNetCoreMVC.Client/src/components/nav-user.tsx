c149bfa7fac5ed4fa15abbc2b496fe8c64e68120
Jordan Murphy <35613487+jordan-d-murphy@users.noreply.github.com>
2025-05-06 00:24:39 -0500

    display user name in nav once logged in

--- ReactTypescriptAspNetCoreMVC.Client/src/components/nav-user.tsx
+++ ReactTypescriptAspNetCoreMVC.Client/src/components/nav-user.tsx
@@ -15,17 +15,17 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "@/auth/useAuth";
 
 export function NavUser({
-  user,
+  testUser,
 }: {
-  user: {
+  testUser: {
     name: string;
     email: string;
     avatar: string;
   };
 }) {
   const { isMobile } = useSidebar();
-  // const { isAuthenticated, logout, user, roles } = useAuth();
-  const { logout } = useAuth();
+  const { isAuthenticated, logout, user, roles } = useAuth();
+  // const { logout } = useAuth();
   const navigate = useNavigate();
   const handleLogout = () => {
     logout();
@@ -44,12 +44,12 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg grayscale">
-                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarImage src={testUser?.avatar} alt={testUser?.name} />
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="text-muted-foreground truncate text-xs">{user.email}</span>
+                <span className="truncate font-medium">{user?.username}</span>
+                <span className="text-muted-foreground truncate text-xs">{user?.email}</span>
               </div>
               <IconDotsVertical className="ml-auto size-4" />
             </SidebarMenuButton>
@@ -63,12 +63,12 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <AvatarImage src={testUser?.avatar} alt={testUser?.name} />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="text-muted-foreground truncate text-xs">{user.email}</span>
+                  <span className="truncate font-medium">{user?.username}</span>
+                  <span className="text-muted-foreground truncate text-xs">{user?.email}</span>
                 </div>
               </div>
             </DropdownMenuLabel>
